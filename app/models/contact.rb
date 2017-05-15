f68b7ee212958ade05341d946c7eaaa624b2549c
Zepri <clem.collongues@gmail.com>
2017-05-15 10:27:00 +0200

    adad

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,9 @@ class Contact < ApplicationRecord
 	attr_accessor :name,      :validate => true
 	attr_accessor :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
    	attribute :nickname,  :captcha  => true
+
+   	validates_presence_of     :name,
+   	validates_presence_of     :email
  
    # Declare the e-mail headers. It accepts anything the mail method
    # in ActionMailer accepts.
