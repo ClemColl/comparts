b23c0fe09049a2d03a065641a10502b2ea7663b3
Zepri <clem.collongues@gmail.com>
2017-05-15 09:38:18 +0200

    fix typo in controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,16 +1,16 @@
 class ContactController < ApplicationController
   def new
- +    @contact = Contact.new
- +  end
- +
- +  def create
- +    @contact = Contact.new(params[:contact])
- +    @contact.request = request
- +    if @contact.deliver
- +      flash.now[:notice] = 'Merci pour votre message!'
- +    else
- +      flash.now[:error] = "Impossible d'envoyer votre message."
- +      render :new
- +    end
- +  end
+     @contact = Contact.new
+   end
+ 
+   def create
+     @contact = Contact.new(params[:contact])
+     @contact.request = request
+     if @contact.deliver
+       flash.now[:notice] = 'Merci pour votre message!'
+     else
+       flash.now[:error] = "Impossible d'envoyer votre message."
+       render :new
+     end
+   end
 end
